5e38b2c82556efd8bf42759d033db60cef5627ad
TheMalachite <eliasgheeraert@gmail.com>
2021-02-03 12:40:31 +0100

    yogurt-vendor: Kang aptX blobs from begonia
    * begonia-user 10 QP1A.190711.020 20.11.18 release-keys
    
    Signed-off-by: SakthivelNadar <s2234nadar@gmail.com>

--- yogurt-vendor.mk
+++ yogurt-vendor.mk
@@ -45,6 +45,8 @@ PRODUCT_COPY_FILES += \
     vendor/micromax/yogurt/proprietary/lib64/libperfctl.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libperfctl.so \
     vendor/micromax/yogurt/proprietary/lib64/libpowerhalwrap.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libpowerhalwrap.so \
     vendor/micromax/yogurt/proprietary/lib64/libpowerhalwrap_jni.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libpowerhalwrap_jni.so \
+    vendor/micromax/yogurt/proprietary/lib64/libaptXHD_encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libaptXHD_encoder.so \
+    vendor/micromax/yogurt/proprietary/lib64/libaptX_encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libaptX_encoder.so \
     vendor/micromax/yogurt/proprietary/vendor/app/EmCamera/lib/arm64/libem_camera_jni.so:$(TARGET_COPY_OUT_VENDOR)/app/EmCamera/lib/arm64/libem_camera_jni.so \
     vendor/micromax/yogurt/proprietary/vendor/app/HorCali/lib/arm64/libprizeem_sensor_jni.so:$(TARGET_COPY_OUT_VENDOR)/app/HorCali/lib/arm64/libprizeem_sensor_jni.so \
     vendor/micromax/yogurt/proprietary/vendor/app/SensorHub/lib/arm64/libem_sensor_jni.so:$(TARGET_COPY_OUT_VENDOR)/app/SensorHub/lib/arm64/libem_sensor_jni.so \
